3d7e38247769abc81a4adaa3f3f9a6e230121a24
Newton <thabangzulu94@gmail.com>
2026-09-24 10:18:17 +0200

    Add pack card frame index

---
+++ src/content/pack-cards.ts
@@ -0,0 +1,57 @@
+import f00 from "./frames/f00";
+import f01 from "./frames/f01";
+import f02 from "./frames/f02";
+import f03 from "./frames/f03";
+import f04 from "./frames/f04";
+import f05 from "./frames/f05";
+import f06 from "./frames/f06";
+import f07 from "./frames/f07";
+import f08 from "./frames/f08";
+import f09 from "./frames/f09";
+import f10 from "./frames/f10";
+import f11 from "./frames/f11";
+import f12 from "./frames/f12";
+import f13 from "./frames/f13";
+import f14 from "./frames/f14";
+import f15 from "./frames/f15";
+import f16 from "./frames/f16";
+import f17 from "./frames/f17";
+import f18 from "./frames/f18";
+import f19 from "./frames/f19";
+import f20 from "./frames/f20";
+import f21 from "./frames/f21";
+import f22 from "./frames/f22";
+import f23 from "./frames/f23";
+import f24 from "./frames/f24";
+import f25 from "./frames/f25";
+import f26 from "./frames/f26";
+
+export const PACK_CARD_FRAMES: string[] = [
+  f00,
+  f01,
+  f02,
+  f03,
+  f04,
+  f05,
+  f06,
+  f07,
+  f08,
+  f09,
+  f10,
+  f11,
+  f12,
+  f13,
+  f14,
+  f15,
+  f16,
+  f17,
+  f18,
+  f19,
+  f20,
+  f21,
+  f22,
+  f23,
+  f24,
+  f25,
+  f26,
+];
